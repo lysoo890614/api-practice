c406ed409150a71fd606dd8cdaafdf24c383ef25
yoonsoo.lee <yoonsoo.lee@bespinglobal.com>
2022-12-13 16:22:11 +0900

    재 배포 13

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,6 +1,6 @@
 DEPLOY_PATH="/var/api-practice"
 DEPLOY_NAME="api-practice-0.0.1-SNAPSHOT.jar"
 
-java -jar ${DEPLOY_PATH}/${DEPLOY_NAME} /dev/null 2> /dev/null < /dev/null &
+java -jar ${DEPLOY_PATH}/${DEPLOY_NAME} > /dev/null 2> /dev/null < /dev/null &
 
 exit 0;
