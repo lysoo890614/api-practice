718c11584a16ad1b396ede8f88b276839aa51c4e
yoonsoo.lee <yoonsoo.lee@bespinglobal.com>
2022-12-14 10:26:42 +0900

    재 배포 17

--- codedeploy/stop_server.sh
+++ codedeploy/stop_server.sh
@@ -1,5 +1,6 @@
 DEPLOY_PATH="/var/api-practice"
 DEPLOY_NAME="api-practice-0.0.1-SNAPSHOT.jar"
 
+sudo su
 ps -ef | grep "${DEPLOY_NAME}" | grep -v grep | awk '{print $2}' | xargs kill -9
-sudo rm -rf ${DEPLOY_PATH}/${DEPLOY_NAME}
+rm -rf ${DEPLOY_PATH}/${DEPLOY_NAME}
